443f38c23326292a3142f6185a2d02b66860ac73
Daniprz <danperezm93@gmail.com>
2024-05-16 16:45:43 +0200

    Agregar la funcion todoitem aparte y colocar propiedades desde arrays para hacerlo dinamico

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import logo from './platzi.webp';
 import './App.css';
 import { TodoCounter } from './TodoCoutner';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList'; 
+import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
+const defaultTodos = [
+{text: 'Verificar la MAJ de la base de datos', completed: false},
+{text: 'Observar los rendimientos de la semana pasada', completed: false},
+{text: 'Optimizar los archivos excel', completed: false},
+{text: 'Escribir codigo para crear paginas web', completed: false},
+{text: 'Terminar el curso de React', completed: false}
+];
 
 function App() {
   return (
-    <div className="App">
-
+    <React.Fragment>
       <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text = {todo.text}
+            completed = {todo.completed} />
+        ))}
+        
       </TodoList>
 
-      {<CreateTodoButton />}
-    </div>
-  );
-}
-
-function TodoItem() {
-  return (
-    <li>
-      <span>V</span>
-      <p> Llorar y lloraaar</p>
-      <span>X</span>
-    </li>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
