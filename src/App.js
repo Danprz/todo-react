e0c94a5a961928df8d084642ff7def464ead78af
Daniprz <danperezm93@gmail.com>
2024-05-16 16:18:03 +0200

    Creando componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './platzi.webp';
 import './App.css';
+import { TodoCounter } from './TodoCoutner';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList'; 
+import { CreateTodoButton } from './CreateTodoButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoCounter completed={16} total={25} />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      {<CreateTodoButton />}
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p> Llorar y lloraaar</p>
+      <span>X</span>
+    </li>
   );
 }
 
